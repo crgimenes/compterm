43675681cf43f6ba3e4a8f7a7b8ad06aac3e1ca0
Hítalo Silva <hitalos@gmail.com>
2023-11-28 00:18:14 -0300

    Improve JS

--- assets/term.js
+++ assets/term.js
@@ -1,66 +1,65 @@
-const termOptions = {
-    fontSize: 20,
-    fontFamily: 'terminal,courier-new,courier,monospace',
-    macOptionClickForcesSelection: true,
-    macOptionIsMeta: true,
-    theme: {
-        background: '#000000',
-        black: '#000000',
-        blue: '#0225c7',
-        brightBlack: '#676767',
-        brightBlue: '#6871ff',
-        brightCyan: '#5ffdff',
-        brightGreen: '#5ff967',
-        brightMagenta: '#ff76ff',
-        brightRed: '#ff6d67',
-        brightWhite: '#fffeff',
-        brightYellow: '#fefb67',
-        cursor: '#adadad',
-        cyan: '#00c5c7',
-        foreground: '#d4d4d4',
-        green: '#00c200',
-        magenta: '#c930c7',
-        red: '#c91b00',
-        white: '#c7c7c7',
-        yellow: '#c7c400',
-    },
-};
+(() => {
 
-const terminal = new Terminal(termOptions);
-terminal.open(document.getElementById('terminal'));
-
-const fitAddon = new FitAddon.FitAddon();
-terminal.resize(80, 24);
-fitAddon.fit();
-
-const progress = '/-\\|';
-let progressIndex = 0;
-
-function connectWS() {
-
-    const ws = new WebSocket(`ws://${window.location.host}/ws`);
-    ws.binaryType = 'blob';
-
-    ws.onopen = () => terminal.reset();
-
-    ws.onmessage = ({ data }) => {
-        const reader = new FileReader();
-        reader.onload = () => terminal.write(new Uint8Array(reader.result));
-        reader.readAsArrayBuffer(data);
+    const termOptions = {
+        fontSize: 20,
+        fontFamily: 'terminal,courier-new,courier,monospace',
+        macOptionClickForcesSelection: true,
+        macOptionIsMeta: true,
+        theme: {
+            background: '#000000',
+            black: '#000000',
+            blue: '#0225c7',
+            brightBlack: '#676767',
+            brightBlue: '#6871ff',
+            brightCyan: '#5ffdff',
+            brightGreen: '#5ff967',
+            brightMagenta: '#ff76ff',
+            brightRed: '#ff6d67',
+            brightWhite: '#fffeff',
+            brightYellow: '#fefb67',
+            cursor: '#adadad',
+            cyan: '#00c5c7',
+            foreground: '#d4d4d4',
+            green: '#00c200',
+            magenta: '#c930c7',
+            red: '#c91b00',
+            white: '#c7c7c7',
+            yellow: '#c7c400',
+        },
     };
 
-    ws.onerror = () => ws.close();
+    const terminal = new Terminal(termOptions);
+    terminal.open(document.getElementById('terminal'));
+    terminal.resize(80, 24);
 
-    ws.onclose = () => {
-        terminal.reset();
-        terminal.write(`\x1b[2J\x1b[0;0HConnection closed.\r\nReconnecting… ${progress[progressIndex]}\r\n`);
-        progressIndex = (progressIndex + 1) % progress.length;
-        setTimeout(connectWS, 1000);
-    };
+    const progress = '/-\\|';
+    let progressIndex = 0;
 
-    terminal.onKey(({ key }) => ws.send(key));
+    function connectWS() {
 
-}
+        const ws = new WebSocket(`ws://${window.location.host}/ws`);
+        ws.binaryType = 'blob';
 
-connectWS();
+        ws.onopen = () => terminal.reset();
 
+        ws.onmessage = ({ data }) => {
+            const reader = new FileReader();
+            reader.onload = () => terminal.write(new Uint8Array(reader.result));
+            reader.readAsArrayBuffer(data);
+        };
+
+        ws.onerror = () => ws.close();
+
+        ws.onclose = () => {
+            terminal.reset();
+            terminal.write(`\x1b[2J\x1b[0;0HConnection closed.\r\nReconnecting… ${progress[progressIndex]}\r\n`);
+            progressIndex = (progressIndex + 1) % progress.length;
+            setTimeout(connectWS, 1000);
+        };
+
+        terminal.onKey(({ key }) => ws.send(key));
+    }
+
+    connectWS();
+
+})();
