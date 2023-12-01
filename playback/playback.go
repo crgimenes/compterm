da1b8cc3d47f50552ecc17a1a844524c92d622b2
crgimenes <crg@crg.eti.br>
2023-12-01 07:48:38 -0300

    do not close if playback file not found

--- playback/playback.go
+++ playback/playback.go
@@ -35,7 +35,8 @@ func (p *Playback) Open() error {
 	var err error
 	p.f, err = os.Open(p.FileName)
 	if err != nil {
-		log.Fatalf("error opening file: %s\r\n", err)
+		log.Printf("error opening file: %s\r\n", err)
+		return fmt.Errorf("error opening file: %s\r\n", err)
 	}
 
 	p.reader = csv.NewReader(p.f)
@@ -79,15 +80,10 @@ func (p *Playback) Play(w io.Writer) error {
 	data := Data{}
 	lastTime := int64(0)
 
-	log.Printf("start playback\r\n")
-
-	i := 0
-
 	for {
 
-		log.Printf("playback %d\r\n", i)
-
 		// TODO: add a way to stop the playback
+		// TODO: Supress stdin/stdout when playing
 
 		// Read one record (until the next delimiter) from csvReader.
 		record, err := p.reader.Read()
@@ -138,9 +134,6 @@ func (p *Playback) Play(w io.Writer) error {
 			log.Printf("error writing to output: %s\r\n", err)
 			return fmt.Errorf("error writing to output: %s\r\n", err)
 		}
-
-		log.Printf("playback %d done\r\n", i)
-		i++
 	}
 
 	return nil
