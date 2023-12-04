ce97c337536dde13c64a186ed7074a675e626c22
crgimenes <crg@crg.eti.br>
2023-12-04 07:37:59 -0300

    implement encode and decode

---
+++ protocol/protocol.go
@@ -0,0 +1,89 @@
+package protocol
+
+import (
+	"encoding/binary"
+	"fmt"
+	"log"
+)
+
+/*
+Protocol format
+XYYYYZZZZZZ...ZZZZZZCCCC
+
+Where:
+
+X: command byte
+YYYY: data length (32 bits, big endian)
+ZZZZZZ...ZZZZZZ: data (array of	bytes)
+CCCC: checksum (FNV-1a, 32 bits, big endian)
+
+The checksum is calculated over the command
+byte, the data length and the data itself.
+*/
+
+const (
+	MAX_DATA_SIZE    = 256 * 1024
+	MAX_PACKAGE_SIZE = 5 + MAX_DATA_SIZE + 4
+)
+
+var (
+	ErrInvalidSize     = fmt.Errorf("invalid data size")
+	ErrInvalidChecksum = fmt.Errorf("invalid checksum")
+)
+
+func checksum(data []byte) uint32 {
+	const (
+		offsetBasis = uint32(2166136261)
+		prime       = 16777619
+	)
+
+	h := offsetBasis
+	for _, b := range data {
+		h ^= uint32(b)
+		h *= 16777619
+	}
+	return h
+}
+
+func Encode(dest []byte, cmd byte, data []byte) (int, error) {
+	lenData := len(data)
+	if lenData > MAX_PACKAGE_SIZE {
+		return 0, ErrInvalidSize
+	}
+	if lenData == 0 {
+		log.Printf("lenData == 0")
+		return 0, nil
+	}
+	if len(dest) < lenData+9 {
+		log.Printf("len(dest) < 5+lenData+4")
+		return 0, ErrInvalidSize
+	}
+
+	dest[0] = cmd
+	binary.BigEndian.PutUint32(dest[1:], uint32(lenData))
+	copy(dest[5:], data)
+	checksum := checksum(dest[0 : 5+lenData])
+	binary.BigEndian.PutUint32(dest[5+lenData:], checksum)
+	n := 9 + lenData
+	return n, nil
+}
+
+func Decode(src []byte) (cmd byte, data []byte, err error) {
+	// command byte + data length + checksum
+	if len(src) < 9 {
+		log.Println("len(src) < 9")
+		return 0, nil, ErrInvalidSize
+	}
+	lenData := int(binary.BigEndian.Uint32(src[1:]))
+	if lenData > MAX_DATA_SIZE {
+		return 0, nil, ErrInvalidSize
+	}
+	if len(src) < 5+lenData+4 {
+		return 0, nil, ErrInvalidSize
+	}
+	h := binary.BigEndian.Uint32(src[5+lenData:])
+	if h != checksum(src[0:5+lenData]) {
+		return 0, nil, ErrInvalidChecksum
+	}
+	return src[0], src[5 : 5+lenData], nil
+}
---
+++ protocol/protocol_test.go
@@ -0,0 +1,106 @@
+package protocol
+
+import (
+	"testing"
+)
+
+func Test_checksum(t *testing.T) {
+	type args struct {
+		data []byte
+	}
+	tests := []struct {
+		name string
+		args args
+		want uint32
+	}{
+		{"empty", args{[]byte{}}, 2166136261},
+		{"a", args{[]byte{'a'}}, 440920331},
+		{"ab", args{[]byte{'a', 'b'}}, 316363613},
+		{"abc", args{[]byte{'a', 'b', 'c'}}, 440920331},
+		{"abcd", args{[]byte{'a', 'b', 'c', 'd'}}, 3459545533},
+		{"abcde", args{[]byte{'a', 'b', 'c', 'd', 'e'}}, 1956368136},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := checksum(tt.args.data); got != tt.want {
+				t.Errorf("checksum(%v) = %v, want %v",
+					string(tt.args.data), got, tt.want)
+			}
+		})
+	}
+}
+
+func TestEncodeDecode(t *testing.T) {
+	in := []byte("hello")
+	out := make([]byte, MAX_PACKAGE_SIZE)
+
+	n, err := Encode(out, 0x01, in)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	cmd, data, err := Decode(out[:n])
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	t.Log("data:", string(data))
+
+	if cmd != 0x01 {
+		t.Errorf("cmd = %v, want 0x01", cmd)
+	}
+
+	if string(data) != string(in) {
+		t.Errorf("data = %v, want %v", string(data), string(in))
+	}
+
+	// test invalid checksum
+	out[5] = 0x00
+	_, _, err = Decode(out[:n])
+	if err != ErrInvalidChecksum {
+		t.Errorf("err = %v, want %v", err, ErrInvalidChecksum)
+	}
+
+	// test invalid size
+	_, _, err = Decode(out[:n-1])
+	if err != ErrInvalidSize {
+		t.Errorf("err = %v, want %v", err, ErrInvalidSize)
+	}
+
+	// test invalid size
+	_, err = Encode(out, 0x01, make([]byte, MAX_DATA_SIZE+1))
+	if err != ErrInvalidSize {
+		t.Errorf("err = %v, want %v", err, ErrInvalidSize)
+	}
+
+	// test invalid size
+	_, err = Encode(out, 0x01, make([]byte, 0))
+	if err != nil {
+		t.Errorf("err = %v, want nil", err)
+	}
+
+	// test invalid size
+	_, err = Encode(out, 0x01, make([]byte, MAX_DATA_SIZE))
+	if err != nil {
+		t.Errorf("err = %v, want nil", err)
+	}
+
+	// test invalid size
+	_, err = Encode(out, 0x01, make([]byte, MAX_DATA_SIZE-1))
+	if err != nil {
+		t.Errorf("err = %v, want nil", err)
+	}
+
+	// test invalid size
+	_, err = Encode(out, 0x01, make([]byte, 1))
+	if err != nil {
+		t.Errorf("err = %v, want nil", err)
+	}
+
+	// test invalid size
+	_, err = Encode(out, 0x01, make([]byte, 0))
+	if err != nil {
+		t.Errorf("err = %v, want nil", err)
+	}
+
+}
