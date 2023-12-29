5f4f1fcdb14f00e6aca70509a74e039ed6b95816
crgimenes <crg@crg.eti.br>
2023-12-29 10:03:11 -0300

    add lua examples

--- assets/init.lua
+++ assets/init.lua
@@ -2,10 +2,37 @@
 -- This file is executed when compterm starts.
 -- You can use this file to load your own lua scripts.
 
-printf = function(s,...)
+Printf = function(s,...)
     return io.write(s:format(...))
 end
 
-configPath = ConfigPath()
-printf("Config path: %s\n", configPath)
+-- ConfigPath
+local path = ConfigPath()
+Printf("Config path: %s\n", path)
+
+-- ConfigDebug
+-- local debug = ConfigDebug(false)
+-- Printf("Config debug: %s\n", debug)
+
+-- ConfigListen
+-- local listen = ConfigListen("0.0.0.0:2200")
+-- Printf("Config listen: %s\n", listen)
+
+-- ConfigAPIListen
+-- local APIListen = ConfigAPIListen("127.0.0.1:2201")
+-- Printf("Config API listen: %s\n", APIListen)
+
+-- ConfigCommand
+-- local command = ConfigCommand(os.getenv("SHELL"))
+-- Printf("Config command: %s\n", command)
+
+-- ConfigMOTD
+-- local MOTD = ConfigMOTD("Welcome to compterm!")
+-- Printf("Config MOTD: %s\n", MOTD)
+
+-- ConfigAPIKey
+-- local APIKey = ConfigAPIKey("") -- some random string
+-- if APIKey == "" then
+--     Printf("Config API key: disabled\n")
+-- end
 
